cb0204b994608d4e6e39466c0ee9310a4707da3e
Diego Montoya <diegomontoyas@gmail.com>
2015-05-01 19:01:06 -0500

    Initial Commit

---
+++ Class-ification Teacher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Class-ification Teacher
+//
+//  Created by Diego on 5/1/15.
+//  Copyright (c) 2015 diegomontoyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
